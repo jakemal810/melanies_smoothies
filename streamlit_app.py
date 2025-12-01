972d7a68812931e0bb26d66b1d2885706059d783
jakemal810 <malionek.jake@gmail.com>
2025-12-01 14:21:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,6 @@ session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:', 
@@ -47,4 +46,4 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
-    
+
